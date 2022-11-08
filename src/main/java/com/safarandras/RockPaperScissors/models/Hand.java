42df2155e20289eaf0b0d80d2cf1ea5070e7b46d
safarandras <safarandras00@gmail.com>
2022-11-08 12:42:18 +0100

    create Hand Enum

---
+++ src/main/java/com/safarandras/RockPaperScissors/models/Hand.java
@@ -0,0 +1,26 @@
+package com.safarandras.RockPaperScissors.models;
+
+public enum Hand {
+    ROCK("Rock"),
+    PAPER("Paper"),
+    SCISSORS("Scissors");
+
+    private final String label;
+
+    Hand(String label){
+        this.label=label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static Hand getHandByLabel(String label){
+        for (Hand hand:Hand.values()){
+            if (hand.getLabel().equalsIgnoreCase(label))
+                return hand;
+        }
+        return null;
+    }
+}
+
