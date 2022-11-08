627e02f2d23a4d30cddc2930e2917b935a93474c
safarandras <safarandras00@gmail.com>
2022-11-08 14:25:03 +0100

    create game controller class

---
+++ src/main/java/com/safarandras/RockPaperScissors/controller/GameController.java
@@ -0,0 +1,12 @@
+package com.safarandras.RockPaperScissors.controller;
+
+import com.safarandras.RockPaperScissors.model.Rule;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GameController {
+}
