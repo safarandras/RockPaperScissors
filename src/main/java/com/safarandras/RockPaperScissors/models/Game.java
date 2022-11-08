619ca9d7d1dcb12d62cf06b97aa6c3e44dac289a
safarandras <safarandras00@gmail.com>
2022-11-08 12:44:04 +0100

    Create game class

---
+++ src/main/java/com/safarandras/RockPaperScissors/models/Game.java
@@ -0,0 +1,29 @@
+package com.safarandras.RockPaperScissors.models;
+
+import Util.RandomUtil;
+
+public class Game {
+    private final Hand playerHand;
+    private final Hand computerHand;
+
+    public Game(String handLabel, RandomUtil random){
+        if (Hand.getHandByLabel(handLabel) == null) throw new IllegalArgumentException();
+        playerHand = Hand.getHandByLabel(handLabel);
+        int handValue = random.nextInt(2);
+        computerHand = Hand.values()[handValue];
+    }
+
+    public boolean hasPlayerWon(){
+        return playerHand == Hand.PAPER && computerHand == Hand.ROCK ||
+                playerHand == Hand.ROCK && computerHand == Hand.SCISSORS ||
+                playerHand == Hand.SCISSORS && computerHand == Hand.PAPER;
+    }
+
+    public Hand getPlayerHand() {
+        return playerHand;
+    }
+
+    public Hand getComputerHand(){
+        return computerHand;
+    }
+}
