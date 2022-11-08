64491642597c34c29cae4a3a5c0e4c6bf07f9e5b
safarandras <safarandras00@gmail.com>
2022-11-08 14:26:39 +0100

    add rules endpoint

--- src/main/java/com/safarandras/RockPaperScissors/controller/GameController.java
+++ src/main/java/com/safarandras/RockPaperScissors/controller/GameController.java
@@ -4,9 +4,12 @@ import com.safarandras.RockPaperScissors.model.Rule;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 public class GameController {
+    @RequestMapping("/rules")
+    public Map<Rule,String> getRules(){
+        return Rule.getAllRules();
+    }
 }
